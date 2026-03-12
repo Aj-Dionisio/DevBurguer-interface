87b1e54d605f7a323f1b246a24c1dbda903f8dbf
Antonio Girotto <jorge_11000@hotmail.com>
2026-03-12 19:10:46 -0300

    Criar index.jsx
    arquivo para o component CategoriesCarousel

---
+++ src/components/index.jsx
@@ -0,0 +1,15 @@
+export function CategoriesCarousel(){
+
+  const [categories, setCategories] = useState([]);
+
+useEffect(()=>{
+ async function loadCategories (){
+    const response = await api.get('/categories')
+console.log(response)
+
+}
+
+loadCategories();
+
+
+},[]},
